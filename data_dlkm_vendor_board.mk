d2bc91c1ed9ff3b31c7243f0e9e8d1988aaa9cd7
LogicFan <logic.fan@outlook.com>
2022-04-24 21:41:27 -0400

    Remove hard-coded kona detecting target board. Some custom ROM (i.e. lineage os) use KONA := kona, which would result in these branch statements giving unexpected results. This PR removes all hard-coded kona used for detecting the TARGET_BOARD_PLATFORM and hence preserves the behavior even if KONA := kona and TARGET_BOARD_PLATFORM := $(KONA).

--- data_dlkm_vendor_board.mk
+++ data_dlkm_vendor_board.mk
@@ -1,6 +1,5 @@
 #Build rmnet perf & shs
 DATA_DLKM_BOARD_PLATFORMS_LIST := msmnile
-DATA_DLKM_BOARD_PLATFORMS_LIST += kona
 DATA_DLKM_BOARD_PLATFORMS_LIST += lito
 ifneq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(call is-board-platform-in-list,$(DATA_DLKM_BOARD_PLATFORMS_LIST)),true)
